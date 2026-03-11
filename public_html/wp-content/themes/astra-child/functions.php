9aff804a4d8843ba59fb2afe8eb5b044bf4ff412
ophelieresfa <resfa.ophelie@yahoo.fr>
2026-03-11 18:58:33 +0100

    Add public_html

---
+++ public_html/wp-content/themes/astra-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+add_action('wp_enqueue_scripts', function() {
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array('parent-style'),
+        wp_get_theme()->get('Version')
+    );
+});
